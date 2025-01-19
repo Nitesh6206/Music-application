e62cf111efb98bdfbd56f56ded3b35456b5930df
Nitesh6206 <niteshsingh6206@example.com>
2025-01-19 22:51:56 +0530

    initial load

--- src/Component/MusicPlayer.jsx
+++ src/Component/MusicPlayer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MusicPlayer = () => {
   const [tracks, setTracks] = useState([]);
@@ -43,6 +43,10 @@ const MusicPlayer = () => {
       setLoading(false);
     }
   };
+  useEffect(() => {
+    musicData()
+  }, [])
+  
 
   return (
     <div>
